f6322d35c5e833cf028cd21d3790bfbaaf3b9dd7
Eef-M <98412601+Eef-M@users.noreply.github.com>
2024-07-16 09:16:08 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"github.com/Eef-M/go-restapi-fiber/controllers/bookcontroller"
+	"github.com/Eef-M/go-restapi-fiber/models"
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	models.ConnectDatabase()
+
+	app := fiber.New()
+
+	api := app.Group("/api")
+	book := api.Group("/books")
+
+	book.Get("/", bookcontroller.Index)
+	book.Get("/:id", bookcontroller.Show)
+	book.Post("/", bookcontroller.Create)
+	book.Put("/:id", bookcontroller.Update)
+	book.Delete("/:id", bookcontroller.Delete)
+
+	app.Listen(":8000")
+}
